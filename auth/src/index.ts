e0d875dd47db7ddc70dff0bf472b33ead3423eaa
Ankit D <ankit.99d@gmail.com>
2023-03-21 21:32:15 +0530

    changes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { RequestValidationError } from "./errors/request-validation-error";
 import { DatabaseConnectionError } from "./errors/database-connection-error";
 import errorHandler from "./middlewares/error-handler";
 import "express-async-errors";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,15 @@ app.use(signinRouter);
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("[listening] AUTH ON 3000");
-});
+async function start() {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("DB Connected");
+  } catch (error) {
+    console.error(error);
+  }
+  app.listen(3000, () => {
+    console.log("[listening] AUTH ON 3000");
+  });
+}
+start();
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -5,25 +5,25 @@ import { RequestValidationError } from "../errors/request-validation-error";
 const router = express.Router();
 
 router.post(
-    "/api/users/signin",
-    [
-        body("email").isEmail().withMessage("Email must be a valid email"),
-        body("password")
-            .trim()
-            .isLength({ min: 4, max: 30 })
-            .withMessage("Password length must be between 4 and 30 characters."),
-    ],
-    (req: Request, res: Response) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
-        const { email, password } = req.body;
-
-        console.log("Creating user..");
-
-        return res.send({});
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be a valid email"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 30 })
+      .withMessage("Password length must be between 4 and 30 characters."),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
     }
+    const { email, password } = req.body;
+
+    console.log("Creating user..");
+
+    return res.send({});
+  }
 );
 
 export { router as signinRouter };
