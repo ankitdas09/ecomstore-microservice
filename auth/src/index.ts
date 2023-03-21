13696b083c2cb910dca72e79c60e5420062fa81e
Ankit D <ankit.99d@gmail.com>
2023-03-21 22:40:20 +0530

    signup routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,11 +5,13 @@ import { DatabaseConnectionError } from "./errors/database-connection-error";
 import errorHandler from "./middlewares/error-handler";
 import "express-async-errors";
 import mongoose from "mongoose";
+import { signupRouter } from "./routes/signup";
 
 const app = express();
 app.use(express.json());
 
 app.use(signinRouter);
+app.use(signupRouter);
 
 app.use(errorHandler);
 
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { User } from "../models/user.model";
+import { BadRequestError } from "../errors/bad-request-error";
+
+const router = express.Router();
+
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be a valid email"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 30 })
+      .withMessage("Password length must be between 4 and 30 characters."),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      console.log("Email in use");
+      throw new BadRequestError("Email in use");
+    }
+    const user = User.build({ email, password });
+    await user.save();
+    return res.status(201).send(user);
+  }
+);
+
+export { router as signupRouter };
